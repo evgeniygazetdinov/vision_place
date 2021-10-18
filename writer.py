aa00c9a22260158aa365b7b0c0fd757904f3c6da
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2021-10-18 08:17:00 +0300

    some shapes

--- writer.py
+++ writer.py
@@ -9,7 +9,7 @@ pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]],  np.int32)
 pts.reshape((-1, 1, 2))
 cv2.polylines(img, [pts], True, (0, 255, 255), 3)
 
-font = cv2.FONT_HERSHEY_SIMPLEX            # font lenght
+font = cv2.FONT_HERSHEY_SIMPLEX            # font lenght/ color
 cv2.putText(img, 'My text', (0, 130), font, 1 ,(200,255,255), 2, cv2.LINE_AA)
 cv2.imshow('image', img)
 cv2.waitKey(0)
