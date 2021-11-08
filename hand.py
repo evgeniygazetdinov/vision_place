5499cb16cca66f367856b8c81cac1e147c5d057f
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2021-11-08 05:27:11 +0300

    connectors

--- hand.py
+++ hand.py
@@ -11,10 +11,9 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
