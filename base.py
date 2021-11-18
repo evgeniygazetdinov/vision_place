11d2b65a97a75eeadc6c60f9918d2c3d11642167
Evgeniy Gazetdinov <e.gazetdinov@litebox.ru>
2021-11-18 06:31:23 +0300

    on open face recognation

--- base.py
+++ base.py
@@ -2,12 +2,26 @@ import sys
 
 
 # TODO add run with args
-def app_runner(my_function):
-    def wrapper():
-        while True:
-            try:
-                my_function()
-            except KeyboardInterrupt:
-                print("Interrupted")
-                sys.exit(0)
-    return wrapper
+def app_runner(argument):
+    """
+    decorator factory
+    wrap decorator
+    stuff will need to do pass args into
+    :param argument:
+    :return:
+    """
+    def decorator(my_function):
+        """
+        base entity
+        :param my_function:
+        :return:
+        """
+        def wrapper():
+            while True:
+                try:
+                    my_function()
+                except KeyboardInterrupt:
+                    print("Interrupted")
+                    sys.exit(0)
+        return wrapper
+    return decorator
